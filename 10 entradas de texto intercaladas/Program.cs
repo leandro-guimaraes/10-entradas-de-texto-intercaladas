f865da197bb6e65560e286de5892efbbc7b39e16
Guest 01 <guest01@alunos.sc.senac.br>
2022-05-17 19:44:36 -0300

    10 textos intercalados por -ou-

---
+++ 10 entradas de texto intercaladas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10_entradas_de_texto_intercaladas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            string[] vetorNomes = new string[10];
+            int[] inomes = new int[10];
+
+            for (int i = 0; i < vetorNomes.Length; i++)
+            {
+                Console.WriteLine("Insira um nome: " +i );
+                vetorNomes[i]= Convert.ToString(Console.ReadLine());
+                Console.Clear();
+            }
+            for(int i = 0; i < vetorNomes.Length; i++)
+            {
+                Console.WriteLine(vetorNomes[i]  +" \r\n -ou-");
+                
+            }
+
+        }
+    }
+}
